61280b34b15eb27bccdfc53a5c086c1fd95ce96a
Eugeny Urminsky <e.urminskiy@quickpay.ru>
2013-10-28 16:38:16 +0700

    refsctoring

--- src/main/java/com/spring/mti/dao/RelTestQueshionDao.java
+++
@@ -1,7 +0,0 @@
-package com.spring.mti.dao;
-
-import com.spring.mti.dao.GenericDao;
-import com.spring.mti.model.RelTestQueshion;
-
-public interface RelTestQueshionDao extends GenericDao<RelTestQueshion, Long> {
-}
--- src/main/java/com/spring/mti/dao/RelTestQueshionDaoImpl.java
+++
@@ -1,5 +0,0 @@
-package com.spring.mti.dao;
-import com.spring.mti.model.RelTestQueshion;
-
-public class RelTestQueshionDaoImpl extends GenericDaoImpl<RelTestQueshion, Long> implements RelTestQueshionDao {
-}
